13b33d4bb31661339fbfca2106a02c0eada1dd1b
nawar-taher <nawar.alshaibany@gmail.com>
2022-06-11 18:01:05 +0200

    init commit

---
+++ js.js
@@ -0,0 +1,13 @@
+
+let container = document.getElementById('container')
+
+
+
+let butn = document.getElementById("press")
+ 
+butn.onclick=function(){
+
+    butn.classList.toggle("clos");
+    container.classList.toggle("excet")
+
+}
